0a8de3f052d2176c862695aced9b98207011fb71
William Fish <william.michael.fish@gmail.com>
2026-02-14 21:15:04 +0000

    BAU: Add nix direnv support

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+          ];
+
+          shellHook = ''
+            echo "Use 'python3 -m http.server' to serve locally"
+          '';
+        };
+      }
+    );
+}
